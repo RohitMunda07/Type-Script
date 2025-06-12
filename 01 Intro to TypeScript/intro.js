dce14b8e8fbf97676e86fc17a75ebbab05f58c34
RohitMunda07 <rohitkumar14818@gmail.com>
2025-06-12 14:27:24 +0530

    first commit

---
+++ 01 Intro to TypeScript/intro.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+var user = {
+    name: "Rohit",
+    age: 20,
+};
+console.log(user.name);
---
+++ 01 Intro to TypeScript/intro.ts
@@ -0,0 +1,6 @@
+console.log("hello world");
+const user = {
+    name: "Rohit",
+    age: 20,
+}
+console.log(user.name);
